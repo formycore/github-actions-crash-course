02c5960c36fa81cbeecab07eda73c09793235011
sandeep <formycore@gmail.com>
2025-09-16 20:56:02 +0530

    Aishwarya Rai Bachan

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Github actions from Aishwarya Rai</p>
+        <p>Github actions from Aishwarya Rai Bachchan</p>
       </header>
     </div>
   );
