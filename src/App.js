2799e9dc0730b7d3acfedad5592aa2497c53b4a0
sandeep <formycore@gmail.com>
2025-09-16 20:22:58 +0530

    removed the youtube content

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Github actions from samantha</p>
-        <a
-          className="App-link"
-          href="https://youtube.com/@FormyCore"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn DevOps
-        </a>
       </header>
     </div>
   );
